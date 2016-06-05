826d73c38c408368f7b4a15a09347af50494c1ee
Maryam Labib <maryamlabib@berkeley.edu>
2016-06-04 23:09:06 -0700

    Add articles_controller#index and articles_controller#show

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,33 @@
 class ArticlesController < ApplicationController
+	
+	def index
+		@articles = Article.all
+	end
+
+	def show
+		@article = Article.find(params[:id])
+	end
+
 	def new
 	end
 
 	def create
-		render plain: params[:article].inspect
+		@article = Article.new(article_params)
+
+		@article.save
+		redirect_to @article
 	end
+
+	# def show
+		# @article_foo = Article.new()
+		# @article_foo.title = params[:title]
+		# @article_foo.save
+	# end
+	
+	private
+
+	def article_params
+		params.require(:article).permit(:title, :text)
+	end
+
 end
