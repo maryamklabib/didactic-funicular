73b7df89ebdb1ba7841e93611ddf3b4163d66cfb
Maryam Labib <maryamlabib@berkeley.edu>
2016-06-06 18:12:41 -0700

    Implement delete functionality for comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,10 +6,17 @@ def create
 	redirect_to article_path(@article)
 end
 
+def destroy
+	@article = Article.find(params[:article_id])
+	@comment = @article.comments.find(params[:id])
+	@comment.destroy
+	redirect_to article_path(@article)
+end
+
 private
 
-def comment_params
-	params.require(:comment).permit(:commenter, :body)
-end
+	def comment_params
+		params.require(:comment).permit(:commenter, :body)
+	end
 
 end
